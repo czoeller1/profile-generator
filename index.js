949db3f751efaa024cbc2f394e306b42279fce82
Charles Zoeller <charles.zoeller1@gmail.com>
2021-02-17 16:57:41 -0700

    added requires

---
+++ index.js
@@ -0,0 +1,6 @@
+const Manager = require("./manager");
+const Engineer = require("./engineer");
+const Intern = require("./intern");
+
+const inquirer = require("inquirer");
+const fs = require("fs");

eaf5e4b71d8b93849c8525ed27c5eaa3acb4aff0
Charles Zoeller <charles.zoeller1@gmail.com>
2021-02-17 17:00:55 -0700

    added html boilerplate as consts

--- index.js
+++ index.js
@@ -4,3 +4,21 @@ const Intern = require("./intern");
 
 const inquirer = require("inquirer");
 const fs = require("fs");
+
+const begin = `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Team Proflie</title>
+    <link
+      type="text/css"
+      rel="stylesheet"
+      href="./dist/style.css"
+    />
+</head>
+<body>`;
+
+const end = `</body>
+</html>`;
